b78795e4e57bc27eac2fcd2c772fce77dce13f0e
Aleksandr Kuznietsov <7276766@gmail.com>
2026-08-11 22:35:25 +0300

    add zone_repo file

---
+++ internal/repository/zone_repo.go
@@ -0,0 +1,56 @@
+package repository
+
+import "database/sql"
+
+type ZoneRepo interface {
+	Create(warehouseID int, name string) (int, error)
+	GetByWarehouse(warehouseID int) ([]Zone, error)
+}
+
+type Zone struct {
+	ID          int
+	WarehouseID int
+	Name        string
+}
+
+type zoneRepo struct {
+	db *sql.DB
+}
+
+func NewZoneRepo(db *sql.DB) ZoneRepo {
+	return &zoneRepo{
+		db: db,
+	}
+}
+
+func (z *zoneRepo) Create(warehouseID int, name string) (int, error) {
+	row := z.db.QueryRow("INSERT INTO zones (warehouse_id, name) VALUES ($1, $2) RETURNING id", warehouseID, name)
+
+	var id int
+	err := row.Scan(&id)
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+func (z *zoneRepo) GetByWarehouse(warehouseID int) ([]Zone, error) {
+	rows, err := z.db.Query("SELECT id, warehouse_id, name FROM zones WHERE warehouse_id = $1", warehouseID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var results []Zone
+	for rows.Next() {
+		var item Zone
+		err := rows.Scan(&item.ID, &item.WarehouseID, &item.Name)
+		if err != nil {
+			return nil, err
+		}
+		results = append(results, item)
+	}
+
+	return results, nil
+}
