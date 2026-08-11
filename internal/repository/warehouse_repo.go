8ad8c1144a5b9b2a513d4803683e2d6abff4c4b2
Aleksandr Kuznietsov <7276766@gmail.com>
2026-08-11 21:52:09 +0300

    add new method

--- internal/repository/warehouse_repo.go
+++ internal/repository/warehouse_repo.go
@@ -4,9 +4,16 @@ import "database/sql"
 
 type WarehouseRepo interface {
 	Create(name, address string) (int, error)
+	GetAll() ([]Warehouse, error)
 }
 type warehouseRepo struct {
 	db *sql.DB
+}
+
+type Warehouse struct {
+	ID      int
+	Name    string
+	Address string
 }
 
 func NewWarehouseRepo(db *sql.DB) WarehouseRepo {
@@ -25,4 +32,24 @@ func (w *warehouseRepo) Create(name, address string) (int, error) {
 	}
 
 	return id, nil
+}
+
+func (w *warehouseRepo) GetAll() ([]Warehouse, error) {
+	rows, err := w.db.Query("SELECT id, name, address FROM warehouses")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var results []Warehouse
+	for rows.Next() {
+		var item Warehouse
+		err := rows.Scan(&item.ID, &item.Name, &item.Address)
+		if err != nil {
+			return nil, err
+		}
+		results = append(results, item)
+	}
+
+	return results, nil
 }
