26d29755249cc7ea06c460677b40b05d00659151
Aleksandr Kuznietsov <7276766@gmail.com>
2026-08-15 01:15:11 +0300

    stock repo update

--- internal/repository/stock_repo.go
+++ internal/repository/stock_repo.go
@@ -1,2 +1,105 @@
 package repository
 
+import (
+	"database/sql"
+	"errors"
+
+	"golang.org/x/tools/go/analysis/passes/nilfunc"
+)
+
+type StockRepo interface {
+	GetByShelf(shelfID int) ([]StockItem, error)
+	MoveStock(fromShelfID int, toSHelfID int, productID int, quantity int, userID int) error
+	AddStock(shelfID int, productID int, quantity int, userID int) error
+}
+
+type StockItem struct {
+	ID        int
+	ShelfID   int
+	ProductID int
+	Quantity  int
+}
+
+type stockRepo struct {
+	db *sql.DB
+}
+
+func NewStockRepo(db *sql.DB) StockRepo {
+	return &stockRepo{
+		db: db,
+	}
+}
+
+func(s *stockRepo) GetByShelf(shelfID int) ([]StockItem, error) {
+	rows, err := s.db.Query("SELECT id, shelf_id, product_id, quantity FROM stock_items WHERE shelf_id = $1", shelfID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var results []StockItem
+	for rows.Next() {
+		var item StockItem
+		err := rows.Scan(&item.ID, &shelfID, &item.ProductID, &item.Quantity)
+		if err != nil {
+			return nil, err
+		}
+		results = append(results, item)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return results, nil
+}
+
+func(s *stockRepo) MoveStock(fromShelfID int, toSHelfID int, productID int, quantity int, userID int) error {
+tx, err := s.db.Begin()
+if err != nil {
+	return err
+}
+defer tx.Rollback()
+
+_, err := tx.Exec("UPDATE stock_items SET quantity = quantity - $1 WHERE shelf_id = $2 AND product_id = $3",
+quantity, fromShelfID, prosuctID,
+)
+if err != nil {
+	return err
+}
+
+_, err = tx.Exec("INSERT INTO shelf_items (shelf_id, product_id, quantity) VALUES ($!, $2, $3) ON CONFLICT (shelf_id, product_id) DO UPDATE SET quantity = stock_items.quantity + EXCLUDED.quantity", toSHelfID, productID, quantity)
+}
+if err != nil {
+	return err
+}
+
+_, err = tx.Exec("INSERT INTO stock_movements (product_id, from_shelf_id, to_shelf_id, quantity, moved_by) VALUES ($1, $2, $3, $4, $5)", productID, fromShelfID, toSHelfID, quantity, userID)
+if err != nil {
+	return err
+}
+
+return tx.Commit()
+
+func(s *stockRepo) AddStock(shelfID int, productID int, quantity int, userID int) error {
+	tx, err := s.db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	_, err = tx.Exec("INSERT INTO stock_items (shelf_id, product_id, quantity)
+	VALUES ($1, $2, $3)
+	ON CONFLICT (shelf_id, product_id)
+	DO UPDATE SET quantity = stock_items.quantity + EXCLUDED.quantity", shelfID, productID, quantity)
+
+	if err != nil {
+		return err
+	}
+
+	_, err := tx.Exec("INSERT INTO stock_movements (product_id, from_shelf_id, to_shelf_id, quantity, moved_by) VALUES ($1, NULL, $2, $3, $4)", prosuctID, fromShelfID, toSHelfID, userID)
+if err != nil {
+	return nil, err
+}
+
+return tx.Commit()
+}
