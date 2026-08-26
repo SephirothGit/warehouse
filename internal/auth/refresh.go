1bb20a1b96cdae72875cc78d8348eb2cef6f056e
Aleksandr Kuznietsov <7276766@gmail.com>
2026-08-26 23:13:33 +0300

    create refresh.go

--- internal/auth/refresh.go
+++ internal/auth/refresh.go
@@ -1,2 +1,21 @@
 package auth
 
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+func GenerateRefreshToken() (string, error) {
+	bytes := make([]byte, 32)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(bytes), nil
+}
+
+func HashToken(token string) string {
+hash := sha256.Sum256([]byte(token))
+return hex.EncodeToString(hash[:])
+}
