364ba043c7aae6ecd0c57b6e6e13be373058d720
Aleksandr Kuznietsov <7276766@gmail.com>
2026-08-28 23:29:46 +0300

    add auth_service

--- internal/service/auth_service.go
+++ internal/service/auth_service.go
@@ -1,2 +1,22 @@
 package service
 
+import "github.com/SephirothGit/warehouse/internal/repository"
+
+type authService struct {
+	userRepo    repository.UserRepo
+	refreshRepo repository.RefreshTokenRepo
+	jwtSecret   []byte
+}
+
+type AuthService interface {
+	Register(email, password string) (int, error)
+}
+
+func NewAuthService(userRepo repository.UserRepo, refreshRepo repository.RefreshTokenRepo, jwtSecret []byte) AuthService {
+	return &authService{
+		userRepo:    userRepo,
+		refreshRepo: refreshRepo,
+		jwtSecret:   jwtSecret,
+	}
+}
+
