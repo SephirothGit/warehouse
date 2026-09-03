1726b78652ec90396a423213c777ecca82bf1f7a
Aleksandr Kuznietsov <7276766@gmail.com>
2026-09-03 22:33:09 +0300

    add auth_handler

--- internal/handler/auth_handler.go
+++ internal/handler/auth_handler.go
@@ -1,1 +1,41 @@
 package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/SephirothGit/warehouse/internal/service"
+)
+
+type AuthHandler struct {
+	authService service.AuthService
+}
+
+type registerRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func NewAuthHandler(authService service.AuthService) *AuthHandler {
+	return &AuthHandler{
+		authService: authService,
+	}
+}
+
+func (h *AuthHandler) RegisterHandler(w http.ResponseWriter, r *http.Request) {
+	var req registerRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	userID, err := h.authService.Register(req.Email, req.Password)
+	if err != nil {
+		http.Error(w, "unable to register user", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]int{"user_id": userID})
+}
