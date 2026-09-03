e89d2aac7504d47fc74a5964b5fb3833987a8a8b
Aleksandr Kuznietsov <7276766@gmail.com>
2026-09-03 21:36:32 +0300

    add stock_service

--- internal/service/stock_service.go
+++ internal/service/stock_service.go
@@ -1,1 +1,31 @@
 package service
+
+import "github.com/SephirothGit/warehouse/internal/repository"
+
+type stockService struct {
+	repo repository.StockRepo
+}
+
+type StockService interface {
+	GetByShelf(shelfID int) ([]repository.StockItem, error)
+	MoveStock(fromShelfID, toShelfID, productID, quantity, userID int) error
+	AddStock(shelfID, productID, quantity, userID int) error
+}
+
+func NewStockService(repo repository.StockRepo) StockService {
+	return &stockService{
+		repo: repo,
+	}
+}
+
+func (s *stockService) GetByShelf(shelfID int) ([]repository.StockItem, error) {
+	return s.repo.GetByShelf(shelfID)
+}
+
+func (s *stockService) MoveStock(fromShelfID, toShelfID, productID, quantity, userID int) error {
+	return s.repo.MoveStock(fromShelfID, toShelfID, productID, quantity, userID)
+}
+
+func (s *stockService) AddStock(shelfID, productID, quantity, userID int) error {
+	return s.repo.AddStock(shelfID, productID, quantity, userID)
+}
