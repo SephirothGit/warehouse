b4b52ca0a2b6d1ce2553f10f5d3c3943e735c6ef
Aleksandr Kuznietsov <7276766@gmail.com>
2026-09-06 23:26:14 +0300

    feat: add product handler

--- internal/handler/product_handler.go
+++ internal/handler/product_handler.go
@@ -1,1 +1,53 @@
 package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/SephirothGit/warehouse/internal/service"
+)
+
+type ProductHandler struct {
+	productService service.ProductService
+}
+
+type createProductRequest struct {
+	SKU  string `json:"sku"`
+	Name string `json:"name"`
+	Unit string `json:"unit"`
+}
+
+func NewProductHandler(productService service.ProductService) *ProductHandler {
+	return &ProductHandler{
+		productService: productService,
+	}
+}
+
+func (h *ProductHandler) CreateHandler(w http.ResponseWriter, r *http.Request) {
+	var req createProductRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	id, err := h.productService.Create(req.SKU, req.Name, req.Unit)
+	if err != nil {
+		http.Error(w, "unable to create product", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]int{"id": id})
+}
+
+func (h *ProductHandler) ListHandler(w http.ResponseWriter, r *http.Request) {
+	products, err := h.productService.GetAll()
+	if err != nil {
+		http.Error(w, "unable to fetch products", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(products)
+}
