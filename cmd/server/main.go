b614685e6a09e08b58a4ea105b0cb9600391d3fd
Aleksandr Kuznietsov <7276766@gmail.com>
2026-09-09 00:03:42 +0300

    feat: add repo, service, handler layers to main file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,8 +5,10 @@ import (
 	"log"
 	"os"
 
+	"github.com/SephirothGit/warehouse/internal/handler"
+	"github.com/SephirothGit/warehouse/internal/repository"
+	"github.com/SephirothGit/warehouse/internal/service"
 	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -15,17 +17,41 @@ func main() {
 	}
 
 	dsn := os.Getenv("DATABASE_URL")
-
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		log.Fatal("unable to open db connection", err)
 	}
 	defer db.Close()
 
-	err = db.Ping()
-	if err != nil {
-		log.Fatal("unable to connect to db", err)
+	if err := db.Ping(); err != nil {
+		log.Fatal("unable to connect to db: ", err)
 	}
+	log.Println("connected to database successfully")
 
-	log.Println("connected to db successfully")
+	jwtSecret := []byte(os.Getenv("JWT_SECRET"))
+
+	userRepo := repository.NewUserRepo(db)
+	refreshRepo := repository.NewRefreshTokenRepo(db)
+	warehouseRepo := repository.NewWarehouseRepo(db)
+	zoneRepo := repository.NewZoneRepo(db)
+	rackRepo := repository.NewRackRepo(db)
+	shelfRepo := repository.NewShelfRepo(db)
+	productRepo := repository.NewProductRepo(db)
+	stockRepo := repository.NewStockRepo(db)
+
+	authService := service.NewAuthService(userRepo, refreshRepo, jwtSecret)
+	warehouseService := service.NewWarehouseService(warehouseRepo)
+	zoneService := service.NewZoneService(zoneRepo)
+	rackService := service.NewRackService(rackRepo)
+	shelfService := service.NewShelfService(shelfRepo)
+	productService := service.NewProductService(productRepo)
+	stockService := service.NewStockService(stockRepo)
+
+	authHandler := handler.NewAuthHandler(authService)
+	warehouseHandler := handler.NewWarehouseHandler(warehouseService)
+	zoneHandler := handler.NewZoneHandler(zoneService)
+	rackHandler := handler.NewRackHandler(rackService)
+	shelfHandler := handler.NewShelfHandler(shelfService)
+	productHandler := handler.NewProductHandler(productService)
+	stockHandler := handler.NewStockHandler(stockService)
 }
