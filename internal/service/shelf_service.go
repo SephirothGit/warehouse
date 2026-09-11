61d0849bf7b79145a685dfd698129fb29e9c9554
Aleksandr Kuznietsov <7276766@gmail.com>
2026-09-11 23:45:08 +0300

    feat: add context

--- internal/service/shelf_service.go
+++ internal/service/shelf_service.go
@@ -1,14 +1,18 @@
 package service
 
-import "github.com/SephirothGit/warehouse/internal/repository"
+import (
+	"context"
+
+	"github.com/SephirothGit/warehouse/internal/repository"
+)
 
 type shelfService struct {
 	repo repository.ShelfRepo
 }
 
 type ShelfService interface {
-	CreateShelf(rackID int, level int) (int, error)
-	GetByRack(rackID int) ([]repository.Shelf, error)
+	CreateShelf(ctx context.Context, rackID int, level int) (int, error)
+	GetByRack(ctx context.Context, rackID int) ([]repository.Shelf, error)
 }
 
 func NewShelfService(repo repository.ShelfRepo) ShelfService {
@@ -17,10 +21,10 @@ func NewShelfService(repo repository.ShelfRepo) ShelfService {
 	}
 }
 
-func (s *shelfService) CreateShelf(rackID int, level int) (int, error) {
-	return s.repo.Create(rackID, level)
+func (s *shelfService) CreateShelf(ctx context.Context, rackID int, level int) (int, error) {
+	return s.repo.Create(ctx, rackID, level)
 }
 
-func (s *shelfService) GetByRack(rackID int) ([]repository.Shelf, error) {
-	return s.repo.GetByRack(rackID)
+func (s *shelfService) GetByRack(ctx context.Context, rackID int) ([]repository.Shelf, error) {
+	return s.repo.GetByRack(ctx, rackID)
 }
