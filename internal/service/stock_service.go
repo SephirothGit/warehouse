b5391858dfb038f79dd9876b97f5a6e496c34726
Aleksandr Kuznietsov <7276766@gmail.com>
2026-09-11 23:54:43 +0300

    feat: add context

--- internal/service/stock_service.go
+++ internal/service/stock_service.go
@@ -1,15 +1,19 @@
 package service
 
-import "github.com/SephirothGit/warehouse/internal/repository"
+import (
+	"context"
+
+	"github.com/SephirothGit/warehouse/internal/repository"
+)
 
 type stockService struct {
 	repo repository.StockRepo
 }
 
 type StockService interface {
-	GetByShelf(shelfID int) ([]repository.StockItem, error)
-	MoveStock(fromShelfID, toShelfID, productID, quantity, userID int) error
-	AddStock(shelfID, productID, quantity, userID int) error
+	GetByShelf(ctx context.Context, shelfID int) ([]repository.StockItem, error)
+	MoveStock(ctx context.Context, fromShelfID, toShelfID, productID, quantity, userID int) error
+	AddStock(ctx context.Context, shelfID, productID, quantity, userID int) error
 }
 
 func NewStockService(repo repository.StockRepo) StockService {
@@ -18,14 +22,14 @@ func NewStockService(repo repository.StockRepo) StockService {
 	}
 }
 
-func (s *stockService) GetByShelf(shelfID int) ([]repository.StockItem, error) {
-	return s.repo.GetByShelf(shelfID)
+func (s *stockService) GetByShelf(ctx context.Context, shelfID int) ([]repository.StockItem, error) {
+	return s.repo.GetByShelf(ctx, shelfID)
 }
 
-func (s *stockService) MoveStock(fromShelfID, toShelfID, productID, quantity, userID int) error {
-	return s.repo.MoveStock(fromShelfID, toShelfID, productID, quantity, userID)
+func (s *stockService) MoveStock(ctx context.Context, fromShelfID, toShelfID, productID, quantity, userID int) error {
+	return s.repo.MoveStock(ctx, fromShelfID, toShelfID, productID, quantity, userID)
 }
 
-func (s *stockService) AddStock(shelfID, productID, quantity, userID int) error {
-	return s.repo.AddStock(shelfID, productID, quantity, userID)
+func (s *stockService) AddStock(ctx context.Context, shelfID, productID, quantity, userID int) error {
+	return s.repo.AddStock(ctx, shelfID, productID, quantity, userID)
 }
