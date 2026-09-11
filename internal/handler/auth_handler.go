01885a7a0704ea618baab13f41efde0de378ad98
Aleksandr Kuznietsov <7276766@gmail.com>
2026-09-12 00:21:43 +0300

    feat: add context

--- internal/handler/auth_handler.go
+++ internal/handler/auth_handler.go
@@ -39,7 +39,7 @@ func (h *AuthHandler) RegisterHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	userID, err := h.authService.Register(req.Email, req.Password)
+	userID, err := h.authService.Register(r.Context(), req.Email, req.Password)
 	if err != nil {
 		http.Error(w, "unable to register user", http.StatusInternalServerError)
 		return
@@ -57,7 +57,7 @@ func (h *AuthHandler) LoginHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	tokens, err := h.authService.Login(req.Email, req.Password)
+	tokens, err := h.authService.Login(r.Context(), req.Email, req.Password)
 	if err != nil {
 		http.Error(w, "invalid email or password", http.StatusUnauthorized)
 		return
@@ -75,7 +75,7 @@ func (h *AuthHandler) RefreshHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	tokens, err := h.authService.RefreshAccessToken(req.RefreshToken)
+	tokens, err := h.authService.RefreshAccessToken(r.Context(), req.RefreshToken)
 	if err != nil {
 		http.Error(w, "invalid or expired refresh token", http.StatusUnauthorized)
 		return
@@ -93,7 +93,7 @@ func (h *AuthHandler) LogoutHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = h.authService.Logout(req.RefreshToken)
+	err = h.authService.Logout(r.Context(), req.RefreshToken)
 	if err != nil {
 		http.Error(w, "unable to logout", http.StatusUnauthorized)
 		return
