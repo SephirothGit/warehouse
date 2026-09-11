069934ab2f1a489dc131094d7c7d856514dab20e
Aleksandr Kuznietsov <7276766@gmail.com>
2026-09-11 23:48:14 +0300

    feat: add context

--- internal/service/product_service.go
+++ internal/service/product_service.go
@@ -1,14 +1,18 @@
 package service
 
-import "github.com/SephirothGit/warehouse/internal/repository"
+import (
+	"context"
+
+	"github.com/SephirothGit/warehouse/internal/repository"
+)
 
 type productService struct {
 	repo repository.ProductRepo
 }
 
 type ProductService interface {
-	Create(sku, name, unit string) (int, error)
-	GetAll() ([]repository.Product, error)
+	Create(ctx context.Context, sku, name, unit string) (int, error)
+	GetAll(ctx context.Context) ([]repository.Product, error)
 }
 
 func NewProductService(repo repository.ProductRepo) ProductService {
@@ -17,10 +21,10 @@ func NewProductService(repo repository.ProductRepo) ProductService {
 	}
 }
 
-func (p *productService) Create(sku, name, unit string) (int, error) {
-	return p.repo.Create(sku, name, unit)
+func (p *productService) Create(ctx context.Context, sku, name, unit string) (int, error) {
+	return p.repo.Create(ctx, sku, name, unit)
 }
 
-func (p *productService) GetAll() ([]repository.Product, error) {
-	return p.repo.GetAll()
+func (p *productService) GetAll(ctx context.Context) ([]repository.Product, error) {
+	return p.repo.GetAll(ctx)
 }
