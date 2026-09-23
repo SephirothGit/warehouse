41cd5e41476121c9a0824df50cc9e4ff044bd5b9
Aleksandr Kuznietsov <7276766@gmail.com>
2026-09-23 22:23:41 +0300

    feat: add jwt test

--- internal/auth/jwt_test.go
+++ internal/auth/jwt_test.go
@@ -1,6 +1,11 @@
 package auth
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
 
 func TestGenerateAndValidateToken(t *testing.T) {
 	secret := []byte("test-secret")
@@ -18,5 +23,26 @@ func TestGenerateAndValidateToken(t *testing.T) {
 
 	if resultUserID != userID {
 		t.Errorf("expected userID %s got %s", userID, resultUserID)
+	}
+}
+
+func TestValidateToken_Expired(t *testing.T) {
+	secret := []byte("test-secret")
+
+	claims := jwt.MapClaims{
+		"sub": "42", 
+		"exp": time.Now().Add(-1 * time.Minute).Unix(),
+		"iat": time.Now().Add(-16 * time.Minute).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(secret)
+	if err != nil {
+		t.Fatalf("unexpected error signing token: %v", err)
+	}
+
+	_, err = ValidateToken(tokenString, secret)
+	if err == nil {
+		t.Error("expected error validating expired token, got nil")
 	}
 }
